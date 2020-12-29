32acd687a9b390a5f933f2c7bfe6788e3dcfcfac
Jordan Wagner <wagnerjj@sunypoly.edu>
2020-12-29 15:23:14 -0600

    Completed registry page

--- src/app/registry/registry.component.ts
+++ src/app/registry/registry.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registry.component.sass']
 })
 export class RegistryComponent implements OnInit {
+  registries = [{
+    name: 'Amazon',
+    link: 'https://www.amazon.com/wedding/dana-brangaitis-jordan-wagner--june-2021/registry/251174NWQ5GQJ'
+  }, {
+    name: 'Create & Barrel',
+    link: 'https://www.crateandbarrel.com/gift-registry/jordan-wagner-and-dana-brangaitis/r6204559'
+  }, {
+    name: 'Williams Sonoma',
+    link: 'https://www.williams-sonoma.com/registry/tj55xznmbn/registry-list.html'
+  }];
 
   constructor() { }
 
